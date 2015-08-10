8579718af739c1e5064ee743cb9587fece9d9c25
Tom Steele <thomasjsteele@gmail.com>
2015-08-09 23:25:53 -0700

    Initial commit

---
+++ drone-whois/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='drone-whois',
+    version='1.0.0',
+    author='Tom Steele',
+    author_email='tom@stacktitan.com',
+    scripts=['bin/drone-whois'],
+    url='https://github.com/lair-framework/drone-whois',
+    license='LICENSE',
+    description='IPWhois tool for Lair.',
+    install_requires=[
+        "pylair >= 0.2",
+        "ipwhois >= 0.10.2",
+        "docopt >= 0.6.2"
+    ],
+)
