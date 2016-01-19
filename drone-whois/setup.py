be4c5495eeea6bc6ae94484b553baaf48c0d282e
Tom Steele <thomasjsteele@gmail.com>
2016-01-19 12:53:30 -0700

    Updates setup.py to not break ipwhois

--- drone-whois/setup.py
+++ drone-whois/setup.py
@@ -10,8 +10,8 @@ setup(
     license='LICENSE',
     description='IPWhois tool for Lair.',
     install_requires=[
-        "pylair >= 1.0.0",
-        "ipwhois >= 0.10.2",
+        "pylair >= 1.0.2",
+        "ipwhois == 0.10.2",
         "docopt >= 0.6.2",
         "netaddr >= 0.7.15"
     ],
